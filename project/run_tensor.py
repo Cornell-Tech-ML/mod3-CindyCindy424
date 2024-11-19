6dccddd87f17e85775eb3c7dde831ba86ca3dba2
Xinling Deng <cindy-deng@foxmail.com>
2024-11-19 10:08:21 -0500

    merge code from previous mod

--- project/run_tensor.py
+++ project/run_tensor.py
@@ -11,6 +11,49 @@ def RParam(*shape):
     r = 2 * (minitorch.rand(shape) - 0.5)
     return minitorch.Parameter(r)
 
+# TODO: Implement for Task 2.5.
+
+class Network(minitorch.Module):
+    def __init__(self, hidden_size):
+        super().__init__()
+        self.layer1 = Linear(2, hidden_size)
+        self.layer2 = Linear(hidden_size, hidden_size)
+        self.layer3 = Linear(hidden_size, 1)
+
+    def forward(self, x):
+        h1 = self.layer1.forward(x).relu()
+        h2 = self.layer2.forward(h1).relu()
+        return self.layer3.forward(h2).sigmoid()
+
+
+# class Linear(minitorch.Module):
+#     def __init__(self, input_size: int, output_size: int) -> None:
+#         super().__init__()
+#         self.weights = RParam(input_size, output_size)
+#         self.bias = RParam(output_size)
+
+#     def forward(self, input_tensor: minitorch.Tensor) -> minitorch.Tensor:
+#         output = input_tensor @ self.weights.value + self.bias.value
+#         return output
+
+
+class Linear(minitorch.Module):
+    def __init__(self, input_size: int, output_size: int) -> None:
+        super().__init__()
+        self.weights = RParam(input_size, output_size)
+        self.bias = RParam(output_size)
+        self.output_size = output_size
+
+    def forward(self, input_tensor: minitorch.Tensor) -> minitorch.Tensor:
+        batch_size, input_size = input_tensor.shape
+        weights_reshaped = self.weights.value.view(1, input_size, self.output_size)
+        input_reshaped = input_tensor.view(batch_size, input_size, 1)
+        output = (weights_reshaped * input_reshaped).sum(1)
+        output = output.view(batch_size, self.output_size)
+        bias_reshaped = self.bias.value.view(self.output_size)
+
+        return output + bias_reshaped
+
 
 def default_log_fn(epoch, total_loss, correct, losses):
     print("Epoch ", epoch, " loss ", total_loss, "correct", correct)
